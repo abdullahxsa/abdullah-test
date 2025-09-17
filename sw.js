23a0a179d7cf41aaa66ef599837bfa370757182d
abdullahxsa <spyabod@gmail.com>
2025-09-17 05:56:45 +0300

    Full PWA project: index.html + manifest + sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-﻿const CACHE = "uber-calc-v2";
-const ASSETS = ["./","./index.html","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
-self.addEventListener("fetch", e => {
-  if (e.request.mode === "navigate" || e.request.destination === "document") {
-    e.respondWith(fetch(e.request).catch(()=>caches.match("./index.html")));
-  } else {
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
-});
+﻿const BASE="/abdullah-test/";
+const CACHE="uber-calc-v1";
+const ASSETS=[BASE,BASE+"index.html",BASE+"manifest.webmanifest",BASE+"sw.js",BASE+"icons/icon-192.png",BASE+"icons/icon-512.png"];
+
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
